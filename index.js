e2ee77cee738580afb30d5df302264ff8bb30fa9
Eric Craft <eacraft13@gmail.com>
2015-12-16 02:58:34 -0800

    Add timeout to watching

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ var r           = require('rethinkdbdash')(config.rethinkdb);
 
 
 
-initializer(function(err, data) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    else {
-        console.log(data);
-        process.exit();
-    }
-});
+setTimeout(function() {
+
+    initializer(function(err, data) {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        else {
+            console.log(data);
+            process.exit();
+        }
+    });
+
+}, 1000 * 60);
