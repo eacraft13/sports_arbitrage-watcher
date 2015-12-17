a5e2d9e47e5e79468828db8ca258f53ce7545f48
Eric Craft <eacraft13@gmail.com>
2015-12-17 02:32:44 -0800

    Make executable

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
+#!/usr/bin/env node
 var initializer = require('./lib/initializer');
 
 
 
-setTimeout(function() {
-
-    initializer(function(err, data) {
-        if (err) {
-            console.log(err);
-            process.exit(1);
-        }
-        else {
-            console.log(data);
-            process.exit();
-        }
-    });
-
-}, 1000 * 60);
+initializer(function(err, data) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    else {
+        // console.log(data);
+        process.exit();
+    }
+});
